49796898b4729f6e20fb30d72ef178cbd0ec94bf
qwertymine3 <qwertymine3@googlemail.com>
2017-02-12 15:19:48 +0000

    Add mountain noise

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
---mgmini:minify({x=15,y=3,z=15})
+--mgmini:minify({x=5,y=3,z=5})
 
 local base = mgnm:auto({
 	ln = {
@@ -35,6 +35,45 @@ local base = mgnm:auto({
 	--]]
 })
 
+local mountains = mgnm:auto({
+	height_limit = {
+		offset =-10,
+		scale = 38,
+		seed = 3,
+		spread = {x=250,y=250,z=250},
+		octaves = 6,
+		persistance = 0.5,
+		lacunarity = 1.5,
+		size = mgnm.mapchunk_size,
+		dims = 2,
+	},
+	t_noise = {
+		offset = 10,
+		scale = 50,
+		seed = 4,
+		spread = {x=150,y=75,z=150},
+		octaves = 6,
+		persistance = 0.5,
+		lacunarity = 1.5,
+		size = mgnm.mapchunk_size,
+		dims = 3,
+	},
+	combiner = function(self, noises)
+		local t = noises.t_noise
+		local limit = noises.height_limit
+		local minp = self.minp
+		for z=minp.z,minp.z+mgnm.mapchunk_size.z-1 do
+		for y=minp.y,minp.y+mgnm.mapchunk_size.y-1 do
+		for x=minp.x,minp.x+mgnm.mapchunk_size.x-1 do
+			self[self:index(x,y,z)] = t[t:index(x,y,z)] - (y - limit[limit:index(x,z)])
+		end
+		end
+		end
+	end,
+	size = mgnm.mapchunk_size,
+	dims = 3,
+})
+
 
 local stone = minetest.get_content_id("default:stone")
 local sand = minetest.get_content_id("default:sand")
@@ -46,6 +85,8 @@ minetest.register_on_generated(function(minp,maxp,seed)
 	v:get_data()
 
 	base:init(minp)
+	mountains:init(minp)
+
 	for z=minp.z,maxp.z do
 	for y=minp.y,maxp.y do
 	for x=minp.x,maxp.x do
@@ -55,6 +96,8 @@ minetest.register_on_generated(function(minp,maxp,seed)
 			else
 				v.data[v:index(x,y,z)] = stone
 			end
+		elseif mountains:get(x,y,z) > 0 then
+			v.data[v:index(x,y,z)] = sand
 		elseif y < 0 then
 			v.data[v:index(x,y,z)] = water
 		end
